ec6129c9042c0b8165fc451b144158db9b8b30f8
Maria Aravena <mane.aravena@live.cl>
2023-05-12 20:29:57 -0400

    Base ejercicio grupal 9

---
+++ eg_9.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class eg_9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese RUT:");
+        String rut = scanner.nextLine();
+        System.out.println("Ingrese nombre:");
+        String nombre = scanner.nextLine();
+        System.out.println("Ingrese dirección:");
+        String direccion = scanner.nextLine();
+        System.out.println("Ingrese comuna:");
+        String comuna = scanner.nextLine();
+        System.out.println("Ingrese número de teléfono:");
+        String telefono = scanner.nextLine();
+    }
+}
